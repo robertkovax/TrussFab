39cc5ab64ccc2851d6c93e646dfa4bdfde01c4cc
Melvin Witte <Melvin.Witte@student.hpi.de>
2017-07-28 13:00:58 +0200

    use symbols for hash keys, use .to_mm for lengths, use multi line string

--- src/utility/scad_export.rb
+++ src/utility/scad_export.rb
@@ -4,25 +4,25 @@ class ScadExport
   end
 
   def self.node_to_scad(path, node)
-    info = { 'vector_array' => [], 'addon_array' => [], 'type_array' => [] }
+    info = { vector_array: [], addon_array: [], type_array: [] }
 
     node.incidents.each do |incident|
       vector = incident.direction.normalize
       if incident.first_node == node
-        info['addon_array'] << "[#{incident.first_elongation_length.to_l.to_s.sub('mm', '').sub('~ ', '')}, \"#{incident.second_node.id}\"0] "
-        info['type_array'] << '\"SNAP\"'
+        info[:addon_array] << "[#{incident.first_elongation_length.to_mm}, \"#{incident.second_node.id}\"0] "
+        info[:type_array] << '\"SNAP\"'
       else
         vector.reverse!
-        info['addon_array'] << "[#{incident.second_elongation_length.to_l.to_s.sub('mm', '').sub('~ ', '')}, \"#{incident.first_node.id}\"0] "
-        info['type_array'] << '\"SNAP\"'
+        info[:addon_array] << "[#{incident.second_elongation_length.to_mm}, \"#{incident.first_node.id}\"0] "
+        info[:type_array] << '\"SNAP\"'
       end
-      info['vector_array'] << vector.to_s.sub('(','[').sub(')',']')
+      info[:vector_array] << "[#{vector.to_a.join(", ")}]"
     end
 
     node.pods.each do |pod|
-      info['addon_array'] << '[(45 - 0 - 10), \"STAND\",0,24,10,60,0] '
-      info['type_array'] << '\"STAND\"'
-      info['vector_array'] << pod.direction.normalize.to_s.sub('(','[').sub(')',']')
+      info[:addon_array] << '[(45 - 0 - 10), \"STAND\",0,24,10,60,0] '
+      info[:type_array] << '\"STAND\"'
+      info[:vector_array] << "#{pod.direction.normalize.to_a.join(", ")}"
     end
 
     filename = path + '/Connector_' + node.id.to_s.rjust(3, '0') + '.scad'
@@ -32,27 +32,27 @@ class ScadExport
   def self.write_node_to_file(filename, id, info, mode)
     file = File.new(filename, 'w')
     export_string =
-        "// adjust filepath to LibSTLExport if neccessary"+"\n" \
-    + "include <#{ProjectHelper.library_directory}/openscad/LibSTLExport.scad>" + "\n" \
-    + "\n" \
-    + "hubID = \"#{id}\";" +"\n" \
-    + "mode = \"#{mode}\";" + "\n" \
-    + "safetyFlag = false;" + "\n" \
-    + "connectorDataDistance = 0;" + "\n" \
-    + "tubeThinning = 1.0;" + "\n" \
-    + "useFixedCenterSize = false;" + "\n" \
-    + "hubCenterSize = 0;" + "\n" \
-    + "printVectorInteger = 8;" + "\n" \
-    + "dataFileVectorArray = [" + "\n" \
-    + info["vector_array"].join(",\n") + "\n" \
-    + "];" + "\n" \
-    + "dataFileAddonParameterArray = [" + "\n" \
-    + info["addon_array"].join(",\n") + "\n" \
-    + "];" + "\n" \
-    + "connectorTypeArray = [" + "\n" \
-    + info["type_array"].join(",\n") + "\n" \
-    + "];" + "\n" \
-    + "drawHub(dataFileVectorArray, dataFileAddonParameterArray, connectorTypeArray);" + "\n" \
+        "// adjust filepath to LibSTLExport if neccessary\n" \
+        "include <#{ProjectHelper.library_directory}/openscad/LibSTLExport.scad>\n" \
+        "\n" \
+        "hubID = \"#{id}\";\n" \
+        "mode = \"#{mode}\";\n" \
+        "safetyFlag = false;\n" \
+        "connectorDataDistance = 0;\n" \
+        "tubeThinning = 1.0;\n" \
+        "useFixedCenterSize = false;\n" \
+        "hubCenterSize = 0;\n" \
+        "printVectorInteger = 8;\n" \
+        "dataFileVectorArray = [\n" \
+        "#{info[:vector_array].join(",\n")}\n" \
+        "];\n" \
+        "dataFileAddonParameterArray = [\n" \
+        "#{info[:addon_array].join(",\n")}\n" \
+        "];\n" \
+        "connectorTypeArray = [\n" \
+        "#{info[:type_array].join(",\n")}\n" \
+        "];\n" \
+        "drawHub(dataFileVectorArray, dataFileAddonParameterArray, connectorTypeArray);\n"
 
     file.write(export_string)
     file.close
