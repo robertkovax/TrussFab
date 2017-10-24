f133c54f19737c60f542009e28f0fbe059d31560
Tim Oesterreich <tim.oesterreich@student.hpi.de>
2017-10-24 16:12:57 +0200

    fix color conversion

--- src/utility/force_to_color_converter.rb
+++ src/utility/force_to_color_converter.rb
@@ -2,18 +2,20 @@
 class ColorConverter
   class << self
     def convert_temp_color(t_color, t_1, t_2)
+      t_color += 1 if t_color < 0
+      t_color -= 1 if t_color > 1
       color = 0
       if 6 * t_color < 1
         color = t_2 + (t_1 - t_2) * 6 * t_color
       elsif 2 * t_color < 1
         color = t_1
       elsif 3 * t_color < 2
-        color = t_2 + (t_1 - t_2) * (2/3 - t_color) * 6
+        color = t_2 + (t_1 - t_2) * (2.0/3.0 - t_color) * 6
       else
         color = t_2
       end
 
-      return color
+      color
     end
 
     def hsl_to_rgb(h, s, l)
@@ -30,27 +32,17 @@ class ColorConverter
 
         h1 = h/360.0
 
-        t_r = h1 + 0.333
-        if t_r > 1
-          t_r -= 1
-        elsif t_r < 1
-          t_r += 1
-        end
+        t_r = h1 + 1.0/3.0
 
         t_g = h1
 
-        t_b = h1 - 0.333
-        if t_b > 1
-          t_b -= 1
-        elsif t_b < 1
-          t_b += 1
-        end
+        t_b = h1 - 1.0/3.0
 
-        r = convert_temp_color(t_r, t_1, t_2)
-        g = convert_temp_color(t_g, t_1, t_2)
-        b = convert_temp_color(t_b, t_1, t_2)
+        r = (convert_temp_color(t_r, t_1, t_2) * 255).round
+        g = (convert_temp_color(t_g, t_1, t_2) * 255).round
+        b = (convert_temp_color(t_b, t_1, t_2) * 255).round
 
-        rgb = [r, g, b]
+        rgb = "##{"%02X" % r}#{"%02X" % g}#{"%02X" % b}"
       end
 
       return rgb
@@ -65,6 +57,7 @@ class ColorConverter
       value = force.abs / 100.0
       value = 0.5 if value > 0.5
       value = 0 if value < 0
+
       # strong negative force will be blue (stretching), strong positive force
       # will be red (compression)
       # In HSL, a hue value of 200 degrees is a light blue color, 360 degrees is red
