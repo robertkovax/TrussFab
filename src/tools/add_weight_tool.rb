9f43af9ffa5b6968c91791f747a6d5e1ed6793ad
Tim Oesterreich <tim.oesterreich@student.hpi.de>
2017-10-26 17:53:50 +0200

    add weight tool

---
+++ src/tools/add_weight_tool.rb
@@ -0,0 +1,31 @@
+class AddWeightTool < Tool
+  GRAVITY = 9.800000190734863
+
+  def initialize(ui = nil)
+    super
+    @mouse_input = MouseInput.new(snap_to_nodes: true, snap_to_surfaces: true)
+  end
+
+  def onLButtonDown(_flags, x, y, view)
+    @mouse_input.update_positions(view, x, y)
+    snapped_object = @mouse_input.snapped_object
+    case snapped_object
+    when Node then addWeightToNode(snapped_object)
+    when Triangle then addWeightToSurface(snapped_object)
+    end
+  end
+
+  def onMouseMove(_flags, x, y, view)
+    @mouse_input.update_positions(view, x, y)
+  end
+
+  def addWeightToNode(node)
+    node.thingy.add_mass(10)
+  end
+
+  def addWeightToSurface(surface)
+    addWeightToNode(surface.first_node)
+    addWeightToNode(surface.second_node)
+    addWeightToNode(surface.third_node)
+  end
+end
