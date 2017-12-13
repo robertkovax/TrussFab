5599b3be6481d0a4ff48d5675532d0b15798501c
Johannes Filter <hi@jfilter.de>
2017-12-13 17:35:39 +0100

    cut out hexagon from top

--- lib/openscad/Hinge/cap.scad
+++ lib/openscad/Hinge/cap.scad
@@ -1,4 +1,5 @@
 include <../Util/text_on.scad>
+use <../Misc/Hexagon.scad>
 
 module draw_hinge_cap(
     cap_height,
@@ -8,7 +9,9 @@ module draw_hinge_cap(
     connector_end_round,
     connector_end_heigth,
     connector_end_extra_round,
-    connector_end_extra_height) {        
+    connector_end_extra_height,
+    cut_out_hex_height,
+    cut_out_hex_d) {        
     difference() {
         union() {
             rotate([-90, 0, 0])
@@ -27,15 +30,20 @@ module draw_hinge_cap(
             // cut out the two holes
             rotate([-90, 0, 0])
             cylinder(cap_height + connector_end_extra_height, hole_size, hole_size);
+            
+            translate([0, cap_height + connector_end_extra_height - cut_out_hex_height / 2 + 0.01, 0])
+            rotate([-90, 0, 0])       
+            Hexagon(cut_out_hex_d, cut_out_hex_height);
              
             translate([0, -4, 0]) // works for cap_height=10
             rotate([-90, 0, 0])
-            text_on_cylinder(label, r1=round_size - 0.5, r2=round_size - 0.5, h=cap_height, center=false);
+            text_on_cylinder(label, r1=round_size - 1, r2=round_size - 1, h=cap_height, size=5);
              
          }
     }
 }
 
 
+
 // for dev only
-draw_hinge_cap(10, "133.789", 12, 3.1, 30/2, 4, 19.5/2, 2);
+draw_hinge_cap(10, "133.789", 12, 3.1, 30/2, 3.7, 19.9/2, 5, 5, 11.5);
--- src/export/presets.rb
+++ src/export/presets.rb
@@ -1,17 +1,19 @@
 module PRESETS
   SIMPLE_HINGE_OPENSCAD = {
-    "depth" => 24.mm, # depth of a hinge part
-    "width" => 100.mm, # not really important because parts that are too much gets cut away anyway
-    "round_size" => 12.mm, # the round part of a hinge part
-    "hole_size" => 3.1.mm, # where the screw goes through
-    "gap_angle" => 45.mm, # the angle for the triangle in the gap
-    "extra_width_for_hinging" => 6.mm, # there needs to be an extra offset so the hinge part can swing fully
-    "gap_height" => 10.mm, # gap of a hinge part
-    "gap_epsilon" => 0.8.mm, # margin of the gap (due to printing issues)
-    "connector_end_round" => (30 / 2).mm,
-    "connector_end_heigth" => 4.mm,
-    "connector_end_extra_round" => (19.5 / 2).mm,
-    "connector_end_extra_height" => 2.mm,
+    'depth' => 24.mm, # depth of a hinge part
+    'width' => 100.mm, # not really important because parts that are too much gets cut away anyway
+    'round_size' => 12.mm, # the round part of a hinge part
+    'hole_size' => 3.1.mm, # where the screw goes through
+    'gap_angle' => 45.mm, # the angle for the triangle in the gap
+    'extra_width_for_hinging' => 6.mm, # there needs to be an extra offset so the hinge part can swing fully
+    'gap_height' => 10.mm, # gap of a hinge part
+    'gap_epsilon' => 0.8.mm, # margin of the gap (due to printing issues)
+    'connector_end_round' => (30 / 2).mm,
+    'connector_end_heigth' => 3.7.mm,
+    'connector_end_extra_round' => (19.9 / 2).mm, # to better connect the bottles
+    'connector_end_extra_height' => 2.mm,
+    'cut_out_hex_height' => 5.mm, # for the screw
+    'cut_out_hex_d' => 11.5.mm,
   }
 
   SIMPLE_HINGE_RUBY = SIMPLE_HINGE_OPENSCAD.clone
