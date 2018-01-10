2e2ca7d37b34254ea7dbf8e343d3e1b47e4cbd42
Johannes Filter <hi@jfilter.de>
2018-01-10 18:47:23 +0100

    adapt presets after witnessing the first 'real' print

--- src/export/presets.rb
+++ src/export/presets.rb
@@ -1,20 +1,22 @@
 module PRESETS
+  normal_hole_size = 3.2.mm # where the screw goes through
+
   SIMPLE_HINGE_OPENSCAD = {
     'depth' => 24.mm, # depth of a hinge part
     'width' => 100.mm, # not really important because parts that are too much gets cut away anyway
     'round_size' => 12.mm, # the round part of a hinge part
     'gap_angle' => 45.mm, # the angle for the triangle in the gap
-    'hole_size_a' => 3.1.mm, # where the screw goes through
-    'hole_size_b' => 3.1.mm, # where the screw goes through
+    'hole_size_a' => normal_hole_size,
+    'hole_size_b' => normal_hole_size,
     'extra_width_for_hinging' => 6.mm, # there needs to be an extra offset so the hinge part can swing fully
     'gap_height' => 10.mm, # gap of a hinge part
     'gap_epsilon' => 0.8.mm, # margin of the gap (due to printing issues)
     'connector_end_round' => (30.0 / 2).mm,
     'connector_end_heigth' => 3.7.mm,
     'connector_end_extra_round' => (19.9 / 2).mm, # to better connect the bottles
-    'connector_end_extra_height' => 2.mm,
+    'connector_end_extra_height' => 4.mm,
     'cut_out_hex_height' => 5.mm, # for the screw
-    'cut_out_hex_d' => 11.mm,
+    'cut_out_hex_d' => 10.6.mm
   }.freeze
 
   ACTUATOR_HINGE_OPENSCAD = SIMPLE_HINGE_OPENSCAD.dup
@@ -29,7 +31,7 @@ module PRESETS
     key.start_with?('connector_end', 'cut_out', 'round_size')
   end
 
-  CAP_RUBY['hole_size'] = 3.1.mm # where the screw goes through
+  CAP_RUBY['hole_size'] = normal_hole_size
 
   SIMPLE_HINGE_RUBY = SIMPLE_HINGE_OPENSCAD.dup
 
