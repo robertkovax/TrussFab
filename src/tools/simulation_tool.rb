d1b0ad5c8babcffcdea33d055dac4903d00e5779
Tim Oesterreich <tim.oesterreich@nexenio.com>
2018-02-06 12:01:43 +0100

    add drag mode when simulation is running

--- src/tools/simulation_tool.rb
+++ src/tools/simulation_tool.rb
@@ -4,7 +4,14 @@ require 'src/simulation/simulation.rb'
 
 class SimulationTool < Tool
   def initialize(ui)
-    super
+    super(ui)
+    @mouse_input = MouseInput.new(snap_to_nodes: true)
+    @move_mouse_input = nil
+
+    @node = nil
+    @start_position = nil
+    @end_position = nil
+    @moving = false
   end
 
   def activate
@@ -28,12 +35,53 @@ class SimulationTool < Tool
     view.invalidate
   end
 
+  def soft_update(view)
+    return unless @moving
+    @start_position = @node.position
+    @end_position = @mouse_input.position
+    @simulation.add_force_to_node(@node, @start_position.vector_to(@end_position))
+    view.invalidate
+  end
+
+  def update(view, x, y)
+    @mouse_input.update_positions(
+      view, x, y, point_on_plane_from_camera_normal: @start_position || nil
+    )
+
+    soft_update(view)
+  end
+
+  def reset
+    @node = nil
+    @start_position = nil
+    @end_position = nil
+    @moving = false
+  end
+
   def onLButtonDown(_flags, x, y, view)
+    update(view, x, y)
+    node = @mouse_input.snapped_object
+    return if node.nil?
+    @moving = true
+    @node = node
+    @start_position = @end_position = @mouse_input.position
   end
 
   def onMouseMove(_flags, x, y, view)
+    update(view, x, y)
+  end
+
+  def onLButtonUp(_flags, x, y, view)
+    update(view, x, y)
+    return unless @moving
+    @end_position = @mouse_input.position
+    reset
   end
 
   def draw(view)
+    soft_update(view)
+    return if @start_position.nil? || @end_position.nil?
+    view.line_stipple = '_'
+    view.draw_lines(@start_position, @end_position)
   end
 end
