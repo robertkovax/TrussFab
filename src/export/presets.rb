c4428e61a9b52a4c3dfc81864c08f9d535a8fb51
Johannes Filter <hi@jfilter.de>
2018-02-15 16:38:53 +0100

    increase radius of protrusion by 0.15mm

--- src/export/presets.rb
+++ src/export/presets.rb
@@ -27,7 +27,7 @@ module PRESETS
     'gap_epsilon' => 0.8.mm, # margin of the gap (due to printing issues)
     'connector_end_round' => (30.0 / 2).mm,
     'connector_end_heigth' => 3.7.mm,
-    'connector_end_extra_round' => (19.9 / 2).mm, # to better connect the bottles
+    'connector_end_extra_round' => (22.9 / 2).mm, # to better connect the bottles
     'connector_end_extra_height' => 7.mm,
     'cut_out_hex_height_a' => default_cut_out_hex_height,
     'cut_out_hex_height_b' => default_cut_out_hex_height,
