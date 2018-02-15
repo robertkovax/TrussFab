13b314357ca0ad23caf69a921920e4988d32b11c
Johannes Filter <hi@jfilter.de>
2018-02-15 16:35:09 +0100

    raise error when exporting with negative values

--- src/export/export_hinge.rb
+++ src/export/export_hinge.rb
@@ -6,7 +6,14 @@
 require 'src/export/presets.rb'
 
 class ExportHinge
-  def initialize(hub_id, a_other_hub_id, b_other_hub_id, a_l1, a_l2, a_l3, b_l1, b_l2, b_l3, connection_angle, a_gap, b_gap, a_with_connector, b_with_connector, presets)
+  def initialize(hub_id, a_other_hub_id, b_other_hub_id,
+    a_l1, a_l2, a_l3, b_l1, b_l2, b_l3, connection_angle,
+    a_gap, b_gap, a_with_connector, b_with_connector, presets)
+    
+    if [a_l1, a_l2, a_l3, b_l1, b_l2, b_l3].any? { |x| x < 0 }
+      raise "l1, l2, and l3 must not be negative: #{[a_l1, a_l2, a_l3, b_l1, b_l2, b_l3]}"
+    end
+
     @hub_id = hub_id
     @a_other_hub_id = a_other_hub_id
     @b_other_hub_id = b_other_hub_id
