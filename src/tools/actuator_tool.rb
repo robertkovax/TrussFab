ba125cb1ccc71e85695349f520351d6670ca6744
Tim Oesterreich <tim.oesterreich@nexenio.com>
2018-03-05 13:27:08 +0100

    make it possible to create actuator links without having an edge first

--- src/tools/actuator_tool.rb
+++ src/tools/actuator_tool.rb
@@ -10,8 +10,7 @@ class ActuatorTool < Tool
 
   def initialize(ui)
     super
-    @mouse_input = MouseInput.new(snap_to_edges: true)
-    @edge = nil
+    @mouse_input = MouseInput.new(snap_to_edges: true, snap_to_nodes: true)
   end
 
   #
@@ -24,9 +23,13 @@ class ActuatorTool < Tool
 
   def onLButtonDown(_flags, x, y, view)
     @mouse_input.update_positions(view, x, y)
-    @edge = @mouse_input.snapped_object
-    return if @edge.nil?
-    create_actuator(view)
+    obj = @mouse_input.snapped_object
+    return if obj.nil?
+    if obj.is_a?(Edge)
+      change_link_to_actuator(view, obj)
+    else
+      create_new_actuator(view, x, y)
+    end
   end
 
   def onMouseMove(_flags, x, y, view)
@@ -37,10 +40,33 @@ class ActuatorTool < Tool
   # Tool logic
   #
 
-  def create_actuator(view)
+  def change_link_to_actuator(view, edge)
     Sketchup.active_model.start_operation('toggle edge to actuator', true)
-    @edge.link_type = 'actuator'
+    edge.link_type = 'actuator'
     view.invalidate
     Sketchup.active_model.commit_operation
+  end
+
+  def create_new_actuator(view, x, y)
+    if @first_position.nil?
+      @first_position = @mouse_input.update_positions(view, x, y)
+    else
+      second_position = @mouse_input.update_positions(view, x, y, point_on_plane_from_camera_normal: @first_position)
+
+      puts 'Create single actuator link'
+      Sketchup.active_model.start_operation('Create actuator link', true)
+      Graph.instance.create_edge_from_points(@first_position,
+                                             second_position,
+                                             link_type: 'actuator')
+      Sketchup.active_model.commit_operation
+      reset
+    end
+  end
+
+  private
+
+  def reset
+    @mouse_input.soft_reset
+    @first_position = nil
   end
 end
