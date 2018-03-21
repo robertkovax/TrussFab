4e17b6c3b5428bb39417397a7656ef871f556ead
Johannes Filter <hi@jfilter.de>
2018-03-21 14:08:48 +0100

    add asset leg

--- src/configuration/configuration.rb
+++ src/configuration/configuration.rb
@@ -42,7 +42,7 @@ module Configuration
   OCTAHEDRON_PATH = (ProjectHelper.asset_directory + '/primitives/octahedron.json').freeze
   DYNAMIC_TETRAHEDRON_PATH = (ProjectHelper.asset_directory + '/primitives/dynamic_tetrahedron.json').freeze
   DYNAMIC_OCTAHEDRON_PATH = (ProjectHelper.asset_directory + '/primitives/dynamic_octahedron.json').freeze
-  ASSETS_LEG_PATH = (ProjectHelper.asset_directory + '/primitives/dynamic_octahedron.json').freeze # TODO: the leg
+  ASSETS_LEG_PATH = (ProjectHelper.asset_directory + '/primitives/leg.json').freeze
 
   BIG_BIG_BOTTLE_NAME = 'Big Big Double Bottle (60cm)'.freeze
   SMALL_BIG_BOTTLE_NAME = 'Small Big Double Bottle (53cm)'.freeze
