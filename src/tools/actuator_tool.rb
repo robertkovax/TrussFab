27979b1e9a6554964b3ddccd5a8665ccc01b2415
Tim Oesterreich <tim.oesterreich@nexenio.com>
2018-04-26 12:40:05 +0200

    find out if we clicked an actuator with actuator tool

--- src/tools/actuator_tool.rb
+++ src/tools/actuator_tool.rb
@@ -8,7 +8,22 @@ class ActuatorTool < PhysicsLinkTool
   end
 
   def onLButtonDown(flags, x, y, view)
-    edge = super(flags, x, y, view)
-    @ui.animation_pane.add_piston(edge.id) unless edge.nil?
+    @mouse_input.update_positions(view, x, y)
+    obj = @mouse_input.snapped_object
+    return if obj.nil?
+    if obj.is_a?(Edge) && obj.link_type == @link_type
+      change_piston_group(obj)
+    else
+      @edge = if obj.is_a?(Edge)
+                change_link_to_physics_link(view, obj)
+              else
+                create_new_physics_link(view, x, y)
+              end
+      @ui.animation_pane.add_piston(@edge.id) unless @edge.nil?
+    end
+  end
+
+  def change_piston_group(edge)
+    p edge.automatic_movement_group
   end
 end
