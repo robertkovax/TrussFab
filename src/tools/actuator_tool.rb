26cb06ba7df20b98c42c29845e0fb9bcd92646e5
Tim Oesterreich <tim.oesterreich@nexenio.com>
2018-05-04 14:40:21 +0200

    make it possible to change a link type to bottle link

--- src/tools/actuator_tool.rb
+++ src/tools/actuator_tool.rb
@@ -1,8 +1,8 @@
-require 'src/tools/physics_link_tool.rb'
+require 'src/tools/link_tool.rb'
 
 # Tool that places an actuator between two hubs or turns an existing edge into
 # an actuator
-class ActuatorTool < PhysicsLinkTool
+class ActuatorTool < LinkTool
   def initialize(ui)
     super(ui, 'actuator')
   end
--- src/tools/generic_physics_link_tool.rb
+++ src/tools/generic_physics_link_tool.rb
@@ -1,8 +1,8 @@
-require 'src/tools/physics_link_tool.rb'
+require 'src/tools/link_tool.rb'
 
 # Tool that places a generic link (i.e. a link that can be used with a custom
 # force function) between two hubs or turns an existing edge into a generic link
-class GenericPhysicsLinkTool < PhysicsLinkTool
+class GenericPhysicsLinkTool < LinkTool
   def initialize(ui)
     super(ui, 'generic')
   end
