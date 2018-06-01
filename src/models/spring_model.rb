4b2a8bd0dbb014ad7464029b495d2613e1b22ebf
dominik.meier <dominik.meier@student.hpi.de>
2018-06-01 11:12:02 +0200

    Use spring material for spring

--- src/models/spring_model.rb
+++ src/models/spring_model.rb
@@ -4,6 +4,6 @@ require 'src/models/physics_link_model.rb'
 class SpringModel < PhysicsLinkModel
   def initialize
     super
-    @material = Sketchup.active_model.materials['pid_material']
+    @material = Sketchup.active_model.materials['spring_material']
   end
 end
