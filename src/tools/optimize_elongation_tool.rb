9f1d02cdd62fd324253e4b47fd1b3be1edac9591
Philipp Otto <philipp.otto2@student.hpi.de>
2018-07-13 13:49:51 +0200

    Prepare tool to optimize elongations

---
+++ src/tools/optimize_elongation_tool.rb
@@ -0,0 +1,9 @@
+require 'src/tools/tool.rb'
+require 'src/export/node_export_algorithm.rb'
+
+# Shortens to selected elongations as much as possible
+class OptimizeElongationTool < Tool
+  def activate
+
+  end
+end
