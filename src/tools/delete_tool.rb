5dfce22ac14f19e8bbc81e9fb641f996820550bd
Philipp Otto <philipp.otto2@student.hpi.de>
2018-07-13 14:04:27 +0200

    Implement delete like in SU 2016

--- src/tools/delete_tool.rb
+++ src/tools/delete_tool.rb
@@ -33,6 +33,36 @@ class DeleteTool < Tool
     @deleting = false
   end
 
+  def activate
+    selection = Sketchup.active_model.selection
+    p selection
+    return if selection.nil? or selection.empty?
+
+    deleted_objects = []
+    selection.each do |entity|
+      next if entity.nil? || entity.deleted?
+
+      type = entity.get_attribute('attributes', :type)
+      id = entity.get_attribute('attributes', :id)
+
+      next if type.nil? || id.nil?
+
+      if type.include? "Link" or type.include? "PidController"
+        edge = Graph.instance.edges[id]
+        deleted_objects.push(edge) if edge
+      end
+
+      if type.include? "Hub"
+        node = Graph.instance.nodes[id]
+        deleted_objects.push(node) if node
+      end
+    end
+
+    deleted_objects.each(&:delete)
+
+    @ui.animation_pane.sync_hidden_status(Graph.instance.actuator_groups)
+  end
+
   private
 
   def delete(x, y, view)
