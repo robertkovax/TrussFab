f7a8af3de5dcf364f206129ada949d199f09bc99
Lukas Rambold <lukas@rambold.io>
2020-01-22 00:13:04 +0100

    moved simulation trigger to own function

--- src/system_simulation/simulation_runner.rb
+++ src/system_simulation/simulation_runner.rb
@@ -14,21 +14,30 @@ class SimulationRunner
   end
 
   def get_hub_time_series(hubIDs, stepSize, mass, constant=50)
-      overrides = "outputFormat='csv',variableFilter='node_pos.*',startTime=0,stopTime=10,stepSize=0.2,springDamperParallel1.c='#{constant}'"
-      # make overrides=outputFormat='csv',variableFilter='node_pos.*',stopTime='20' simulate
-      command = "make overrides=#{overrides} simulate"
-      puts(command)
-      Open3.popen2e(command, :chdir => @directory) do |i, o, t|
-        o.each {|l| puts l }
-        status = t.value
-      end
+    _run_simulation(constant, "node_pos.*")
   end
 
-  def get_period()
+  def _run_simulation(constant, filter="*")
+    overrides = "outputFormat='csv',variableFilter='#{filter}',startTime=0,stopTime=10,stepSize=0.2,springDamperParallel1.c='#{constant}'"
+    # make overrides=outputFormat='csv',variableFilter='node_pos.*',stopTime='20' simulate
+    command = "make overrides=#{overrides} simulate"
+    puts(command)
+    Open3.popen2e(command, :chdir => @directory) do |i, o, t|
+      o.each {|l| puts l }
+      status = t.value
+    end
+  end
 
-  end 
+  def get_period(mass=70, constant=50)
+    _run_simulation("revLeft.phi")
+    @data = ModellicaExport.import_csv("seesaw3_res.csv")
+    print @data
+    my_array = [[1,2,3.5,4],[3,5.2,7,22]]
+    my_fft = my_array.fft
+    print my_fft
+  end
 
 end
 
-#runner = SimulationRunner.new
-#runner.get_hub_time_series(nil, 0, 0)
+runner = SimulationRunner.new
+runner.get_period(70,60)
