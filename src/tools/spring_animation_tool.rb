f23fc1727226696cb2fbe54f29bfa6b3b7a1f763
birneamstiel <luckifri@yahoo.de>
2020-02-20 14:05:16 +0100

    Extrac trace visualization into own class.

--- src/tools/spring_animation_tool.rb
+++ src/tools/spring_animation_tool.rb
@@ -4,9 +4,10 @@ require 'src/system_simulation/modellica_export.rb'
 require 'src/geometry_animation.rb'
 require 'src/trace_animation.rb'
 require 'src/system_simulation/simulation_runner.rb'
+require 'src/system_simulation/trace_visualization.rb'
 
+# TODO rename to place user tool, delete old one and add documentation here.
 class SpringAnimationTool < Tool
-  INTERACT_HTML_FILE = '../ui/spring-interact/index.html'.freeze
   INSIGHTS_HTML_FILE = '../ui/spring-insights/index.html'.freeze
 
   def initialize(ui)
@@ -29,11 +30,9 @@ class SpringAnimationTool < Tool
     # Spring constant
     @constant = 20000
 
-    # Visualizing animation samples by plotting a circle.
-    @trace_points = []
+    # Visualize oscillation as a trace, will be instantiated with simulation data.
+    @trace_visualization = nil
 
-    # Group containing trace circles.
-    @group = Sketchup.active_model.active_entities.add_group
   end
 
   def activate
@@ -58,11 +57,12 @@ class SpringAnimationTool < Tool
       set_graph_to_data_sample(0)
 
       # Visualize for current spring constant.
-      add_circle_trace(["18", "20"], 4)
+      @trace_visualization = TraceVisualization.new(@simulation_data)
+      @trace_visualization.add_trace(["18", "20"], 4)
 
     else
       # Reset trace visualization.
-      reset_trace
+      @trace_visualization.reset_trace
 
       # Stop Animation.
       toggle_animation
@@ -123,113 +123,6 @@ class SpringAnimationTool < Tool
   end
 
   #
-  # Trace logic
-  #
-  #
-  def add_sphere_trace(node_ids, sparse_factor)
-    @simulation_data.each_with_index do |current_data_sample, index|
-      # thin out points in trace
-      next unless index % sparse_factor == 0
-
-      @group = Sketchup.active_model.entities.add_group if @group.deleted?
-      entities = @group.entities
-
-      color = Sketchup::Color.new(72,209,204)
-      materialToSet = Sketchup.active_model.materials.add("MyColor_1")
-      materialToSet.color = color
-      materialToSet.alpha = 0.2
-
-      radius = 1
-      num_segments = 20
-      circle = entities.add_circle(current_data_sample.position_data[node_ids[0]], Geom::Vector3d.new(1,0,0), radius, num_segments)
-      face = entities.add_face(circle)
-      face.material = materialToSet unless face.deleted?
-      face.back_material = materialToSet unless face.deleted?
-      face.reverse!
-      # Create a temporary path for follow me to use to perform the revolve.
-      # This path should not touch the face.
-      path = entities.add_circle(current_data_sample.position_data[node_ids[0]], Geom::Vector3d.new(0,0,1), radius * 2, num_segments)
-      # This creates the sphere.
-      face.followme(path)
-
-      entities.erase_entities(path)
-
-
-      circle = entities.add_circle(current_data_sample.position_data[node_ids[1]], Geom::Vector3d.new(1,0,0), radius, num_segments)
-      face = entities.add_face(circle)
-      face.material = materialToSet unless face.deleted?
-      face.back_material = materialToSet unless face.deleted?
-      face.reverse!
-      # Create a temporary path for follow me to use to perform the revolve.
-      # This path should not touch the face.
-      path = entities.add_circle(current_data_sample.position_data[node_ids[1]], Geom::Vector3d.new(0,0,1), radius * 2, num_segments)
-      # This creates the sphere.
-      face.followme(path)
-
-      entities.erase_entities(path)
-
-      #    entities.grep(Sketchup::Edge).each{|e| e.hidden=true }
-    end
-  end
-
-  def add_circle_trace(node_ids, sparse_factor)
-    @simulation_data.each_with_index do |current_data_sample, index|
-      # thin out points in trace
-      next unless index % sparse_factor == 0
-
-      @group = Sketchup.active_model.entities.add_group if @group.deleted?
-      entities = @group.entities
-
-      color = Sketchup::Color.new(72,209,204)
-      materialToSet = Sketchup.active_model.materials.add("MyColor_1")
-      materialToSet.color = color
-      materialToSet.alpha = 0.4
-
-      edgearray = entities.add_circle(current_data_sample.position_data[node_ids[0]], Geom::Vector3d.new(1,0,0), 1, 10)
-      edgearray.each{|e| e.hidden=true }
-      first_edge = edgearray[0]
-      arccurve = first_edge.curve
-      face = entities.add_face(arccurve)
-      face.material = materialToSet unless face == nil
-
-      edgearray = entities.add_circle(current_data_sample.position_data[node_ids[1]], Geom::Vector3d.new(1,0,0), 1, 10)
-      edgearray.each{|e| e.hidden=true }
-      first_edge = edgearray[1]
-      arccurve = first_edge.curve
-      face = entities.add_face(arccurve)
-      face.material = materialToSet unless face == nil
-
-      #    entities.grep(Sketchup::Edge).each{|e| e.hidden=true }
-    end
-  end
-
-  def add_sparse_trace(node_ids, sparse_factor)
-    @simulation_data.each_with_index do |current_data_sample, index|
-      # thin out points in trace
-      next unless index % sparse_factor == 0
-
-      model = Sketchup.active_model
-      entities = model.active_entities
-      @trace_points << entities.add_cpoint(current_data_sample.position_data[node_ids[0]])
-      @trace_points << entities.add_cpoint(current_data_sample.position_data[node_ids[1]])
-      #puts(current_data_sample.time_stamp)
-    end
-  end
-
-  def add_trace(node_ids)
-    add_sparse_trace(node_ids, 100)
-  end
-
-  def reset_trace()
-    @group = Sketchup.active_model.entities.add_group if @group.deleted?
-    Sketchup.active_model.active_entities.erase_entities(@group.entities.to_a)
-    if @trace_points.count > 0
-      Sketchup.active_model.active_entities.erase_entities(@trace_points)
-    end
-    @trace_points = []
-  end
-
-  #
   # Dialog logic
   #
   #
@@ -259,13 +152,12 @@ class SpringAnimationTool < Tool
   def register_insights_callbacks
     @insights_dialog.add_action_callback('spring_insights_change') do |_, value|
       puts(value)
-      reset_trace()
+      @trace_visualization.reset_trace
       @constant = value
       simulate
-      drawing_time = Benchmark.realtime { add_circle_trace(["18", "20"], 4) }
+      drawing_time = Benchmark.realtime { @trace_visualization.add_trace(["18", "20"], 4) }
       puts("drawing time: " + drawing_time.to_s + "s")
       #get_period(value)
-      #@animation.factor = @animation.factor + 2
     end
 
     @insights_dialog.add_action_callback('spring_insights_toggle_play') do |_, value|
