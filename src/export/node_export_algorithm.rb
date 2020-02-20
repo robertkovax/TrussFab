488d5ab7fb373fa731cf42e08b420143b5a04929
dominik.meier <dominik.meier@student.hpi.de>
2020-02-20 15:43:17 +0100

    Add insetting for rotary edges (WIP)

--- src/export/node_export_algorithm.rb
+++ src/export/node_export_algorithm.rb
@@ -25,8 +25,11 @@ class NodeExportAlgorithm
 
     static_groups = StaticGroupAnalysis.find_static_groups
 
-    check_for_only_simple_hinges static_groups
+    rotary_hinges_ids = check_for_only_simple_hinges static_groups
 
+    offset_rotary_hinge_hubs(rotary_hinges_ids, static_groups)
+
+    return
     static_groups.select! { |group| group.size > 1 }
     static_groups.sort! { |a, b| b.size <=> a.size }
     static_groups = prioritise_pod_groups(static_groups)
@@ -85,6 +88,61 @@ class NodeExportAlgorithm
 
   private
 
+  def offset_rotary_hinge_hubs(rotary_hinges, static_groups)
+    static_groups_as_sets_of_nodes = static_groups.map do |triangles|
+      nodes = Set.new
+      triangles.each do |triangle|
+        nodes << triangle.first_node
+        nodes << triangle.second_node
+        nodes << triangle.third_node
+      end
+      nodes
+    end
+
+    rotary_hinges.each do |node_pair|
+      static_groups_pair = static_groups_as_sets_of_nodes.select { |node_set| node_set.include?(node_pair[0]) && node_set.include?(node_pair[1]) }
+
+      substructure_to_inset = choose_structure_to_inset static_groups_pair.to_a
+
+      node_pair.each do |node_to_inset|
+        other_node_to_inset = if node_pair[0] == node_to_inset
+                                node_pair[1]
+                              else
+                                node_pair[0]
+                              end
+
+        edges_from_hinge_into_inset = node_to_inset.incidents.select { |edge| substructure_to_inset.include? edge.other_node(node_to_inset) }.select { |edge| edge.opposite(node_to_inset) != other_node_to_inset }
+        nodes_to_reconnect_to = edges_from_hinge_into_inset.map { |edge| edge.opposite(node_to_inset) }
+
+        edges_from_hinge_into_inset.each(&:delete)
+
+        inset_vector = node_to_inset.position.vector_to(other_node_to_inset.position)
+        inset_vector.length = 100.mm
+        inset_position = node_to_inset.position + inset_vector
+
+        nodes_to_reconnect_to.each do |node|
+          Graph.instance.create_edge_from_points(inset_position, node.position)
+        end
+      end
+    end
+  end
+
+  def choose_structure_to_inset(static_groups_pair)
+    # 1. Criterion: Offset which has no pods
+    has_pods = static_groups_pair.map do |static_group|
+      static_group.to_a.any? { |node| node.pods.size > 0 }
+    end
+    return static_groups_pair[0] if !has_pods[0] && has_pods[1]
+    return static_groups_pair[1] if has_pods[0] && !has_pods[1]
+
+    # 2. Criterion: Number of nodes
+    if static_groups_pair[0].size > static_groups_pair[1].size
+      static_groups_pair[1]
+    else
+      static_groups_pair[0]
+    end
+  end
+
   def edge_angle(edge1, edge2)
     edge1.direction.normalize.dot(edge2.direction.normalize).abs
   end
@@ -92,24 +150,30 @@ class NodeExportAlgorithm
   # Make sure that if static groups touch, they touch at exactly 2 points,
   # otherwise, the structure will be bended, and not fabricateable with welding
   # (statement yet to proof)
-  def check_for_only_simple_hinges static_groups
-    static_groups_as_sets_of_node_ids = static_groups.map do |triangles|
-      node_ids = Set.new
+  def check_for_only_simple_hinges(static_groups)
+    rotary_hinges = []
+    static_groups_as_sets_of_nodes = static_groups.map do |triangles|
+      nodes = Set.new
       triangles.each do |triangle|
-        node_ids << triangle.first_node.id
-        node_ids << triangle.second_node.id
-        node_ids << triangle.third_node.id
+        nodes << triangle.first_node
+        nodes << triangle.second_node
+        nodes << triangle.third_node
       end
-      puts node_ids
-      node_ids
+      nodes
     end
-    static_groups_as_sets_of_node_ids.product(static_groups_as_sets_of_node_ids) do |one, two|
+    static_groups_as_sets_of_nodes.product(static_groups_as_sets_of_nodes) do |one, two|
       next if one == two
-      difference_size = (one & two).size
-      unless [0, 2].include? difference_size
+
+      difference = (one & two)
+      unless [0, 2].include? difference.size
         puts "Structure has hinges that won't be buildable "
       end
+      if difference.size == 2
+        difference_array = difference.to_a
+        rotary_hinges.push([difference_array[0], difference_array[1]])
+      end
     end
+    rotary_hinges
   end
 
 
