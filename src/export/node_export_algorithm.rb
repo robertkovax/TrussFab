a3d63d0b205409c72b28208ee2c59812db9fded6
dominik.meier <dominik.meier@student.hpi.de>
2020-02-21 17:31:17 +0100

    Disable export

--- src/export/node_export_algorithm.rb
+++ src/export/node_export_algorithm.rb
@@ -23,68 +23,66 @@ class NodeExportAlgorithm
     nodes = Graph.instance.nodes.values
     edges = Graph.instance.edges.values
 
-    constant_static_groups = StaticGroupAnalysis.find_static_groups
+    static_groups = StaticGroupAnalysis.find_static_groups
 
-    static_groups = constant_static_groups.clone
+    rotary_hinge_pairs = check_for_only_simple_hinges static_groups
+    offset_rotary_hinge_hubs(rotary_hinge_pairs, static_groups)
 
-
-    static_groups.select! { |group| group.size > 1 }
-    static_groups.sort! { |a, b| b.size <=> a.size }
-    static_groups = prioritise_pod_groups(static_groups)
-
-    @export_interface = NodeExportInterface.new(static_groups)
-
-    group_edge_map = {}
-
-    # generate hubs for all groups with size > 1
-    static_groups.each do |group|
-      group_nodes = Set.new(group.flat_map(&:nodes))
-      group_edges = Set.new(group.flat_map(&:edges))
-
-      group_edge_map[group] = group_edges
-
-      group_nodes.each do |node|
-        hub_edges = group_edges.select { |edge| edge.nodes.include? node }
-        hub_edges = sort_edges_clockwise(hub_edges)
-
-        hub = HubExportInterface.new(hub_edges)
-        @export_interface.add_hub(node, hub)
-      end
-    end
+    # static_groups.select! { |group| group.size > 1 }
+    # static_groups.sort! { |a, b| b.size <=> a.size }
+    # static_groups = prioritise_pod_groups(static_groups)
+    #
+    # @export_interface = NodeExportInterface.new(static_groups)
+    #
+    # group_edge_map = {}
+    #
+    # # generate hubs for all groups with size > 1
+    # static_groups.each do |group|
+    #   group_nodes = Set.new(group.flat_map(&:nodes))
+    #   group_edges = Set.new(group.flat_map(&:edges))
+    #
+    #   group_edge_map[group] = group_edges
+    #
+    #   group_nodes.each do |node|
+    #     hub_edges = group_edges.select { |edge| edge.nodes.include? node }
+    #     hub_edges = sort_edges_clockwise(hub_edges)
+    #
+    #     hub = HubExportInterface.new(hub_edges)
+    #     @export_interface.add_hub(node, hub)
+    #   end
+    # end
 
     # put hinges everywhere possible
-    triangles = Set.new(edges.flat_map(&:adjacent_triangles))
+    # triangles = Set.new(edges.flat_map(&:adjacent_triangles))
+    #
+    # triangles.each do |tri|
+    #   tri.edges.combination(2).each do |e1, e2|
+    #     hinge = generate_hinge_if_necessary(
+    #       e1, e2, tri, static_groups, group_edge_map
+    #     )
+    #
+    #     node = e1.shared_node(e2)
+    #
+    #     if !@export_interface.has_mainhub_at_node(node) &&
+    #        !hinge.is_double_hinge
+    #       hub = HubExportInterface.new([e1, e2])
+    #       @export_interface.add_hub(node, hub)
+    #     else
+    #       @export_interface.add_hinge(node, hinge) unless hinge.nil?
+    #     end
+    #   end
+    # end
 
-    triangles.each do |tri|
-      tri.edges.combination(2).each do |e1, e2|
-        hinge = generate_hinge_if_necessary(
-          e1, e2, tri, static_groups, group_edge_map
-        )
+    # @export_interface.apply_hinge_algorithm
 
-        node = e1.shared_node(e2)
+    # Sketchup.active_model.start_operation('elongate edges', true)
+    # ElongationManager.improve_elongations(@export_interface, nodes, false)
+    # Sketchup.active_model.commit_operation
 
-        if !@export_interface.has_mainhub_at_node(node) &&
-           !hinge.is_double_hinge
-          hub = HubExportInterface.new([e1, e2])
-          @export_interface.add_hub(node, hub)
-        else
-          @export_interface.add_hinge(node, hinge) unless hinge.nil?
-        end
-      end
-    end
+    # Sketchup.active_model.start_operation('visualize export result', true)
+    # NodeExportVisualization.visualize(@export_interface)
+    # Sketchup.active_model.commit_operation
 
-    @export_interface.apply_hinge_algorithm
-
-    Sketchup.active_model.start_operation('elongate edges', true)
-    ElongationManager.improve_elongations(@export_interface, nodes, false)
-    Sketchup.active_model.commit_operation
-
-    Sketchup.active_model.start_operation('visualize export result', true)
-    NodeExportVisualization.visualize(@export_interface)
-    Sketchup.active_model.commit_operation
-
-    rotary_hinge_pairs = check_for_only_simple_hinges constant_static_groups
-    offset_rotary_hinge_hubs(rotary_hinge_pairs, constant_static_groups)
   end
 
   private
@@ -127,7 +125,8 @@ structures at the same hinge"
         inset_position = node_to_inset.position + inset_vector
 
         nodes_to_reconnect_to.each do |node_information|
-          Graph.instance.create_edge_from_points(inset_position, node_information[:node].position, link_type: node_information[:type])
+          edge = Graph.instance.create_edge_from_points(inset_position, node_information[:node].position, link_type: node_information[:type])
+
         end
       end
     end
