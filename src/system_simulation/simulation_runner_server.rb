2804690859df5d1109aa98a2fbb83eac5cff83d0
birneamstiel <luckifri@yahoo.de>
2020-03-10 17:08:17 +0100

    Exted simulation runner server interace.

--- src/system_simulation/simulation_runner_server.rb
+++ src/system_simulation/simulation_runner_server.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'sinatra'
-require './simulation_runner.rb'
-require "./generate_modelica_model.rb"
+require_relative 'simulation_runner.rb'
+require_relative 'generate_modelica_model.rb'
 
 set :port, 8080
 set :environment, :production
@@ -10,10 +10,31 @@ sim = nil
 
 post '/update_model' do
   sim = SimulationRunner.new_from_json_export(request.body.read)
+  p sim
+  return ""
+end
+
+patch '/update_spring_constants' do
+  sim.update_spring_constants(JSON.parse(request.body.read))
   return ""
 end
 
 get '/get_period' do
-  return_message = {'period' => sim.get_period().to_s}
+  return_message = { period: sim.get_period.to_s }
+  return_message.to_json
+end
+
+get '/get_hub_time_series' do
+  return_message = { data: sim.get_hub_time_series }
+  return_message.to_json
+end
+
+get '/get_equilibrium' do
+  return_message = { equilibrium: sim.get_period.to_s }
+  return_message.to_json
+end
+
+get '/get_constant_for_constrained_angle' do
+  return_message = { constant: sim.get_period.to_s }
   return_message.to_json
 end
