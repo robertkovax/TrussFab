5fac23a367d6a78ed529f07500ef84660e772232
Lukas Rambold <lukas@rambold.io>
2020-04-06 18:55:00 +0200

    modelica file parsing for linearized models

--- src/system_simulation/server/linear_state_space_model.rb
+++ src/system_simulation/server/linear_state_space_model.rb
@@ -1,26 +1,68 @@
 require 'gsl'
 
 def is_matrix(line)
-  /^  parameter Real [A-D]/.match(line)
+  /^  parameter Real [A-D]/.match?(line)
+end
+
+def is_vector(line)
+  /^  parameter Real [ux]0/.match?(line)
 end
 
 def is_label(line)
-  /^  Real/.match(line)
+  /^  Real \'/.match?(line)
 end
 
 class LinearStateSpaceModel
   def initialize(path)
+
     File.readlines(path).each do |line|
       # TODO parse initial condition
-      if is_label(line)
-        # TODO parse lables
-        puts line
-      elsif is_matrix(line)
-        matrix_name, matrix_data = /^  parameter Real ([A-D]).*= \[(.*)\];$/.match(line).captures
-        puts matrix_data
-        mat = line.split("; ").map{|row| row.split(", ").map{ |cell| cell.to_f }.to_gv}
-        # TODO finishe GSL to Matrix parsing
+      if is_matrix(line)
+
+        is_zero = false
+        line_match = /^  parameter Real (?<matrix_name>[A-D]).*= \[(?<matrix_data>.*)\];$/.match(line)
+        if line_match
+          mat = line_match[:matrix_data].split("; ").map{|row| row.split(", ").map{ |cell| cell.to_f }}
+          store_matrix(line_match[:matrix_name], GSL::Matrix[*mat])
+        else
+          # if a normal match cannot be made we will check whether it is a empty matrix
+          line_match = /^  parameter Real (?<matrix_name>[A-D]).*= zeros\(., .\);$/.match(line)
+          if line_match
+            is_zero = true
+          else
+            raise "Invalid Matrix format in linearized Modelica output file"
+          end
+        end
+          # TODO generate empty matrix
+      # TODO finishe GSL to Matrix parsing
+
+      elsif is_vector(line)
+        line_match = /^  parameter Real (?<vector_name>[ux]0).*= \{(?<vector_data>.*)\};$/.match(line)
+        vec = line_match[:vector_data].split(', ').map{|cell| cell.to_f }
+        store_vector(line_match[:vector_name], GSL::Vector[*vec])
+      elsif is_label(line)
+        line_match = /^  Real \'(?<label_name>.*)\' = (?<label_category>[ux])\[(?<label_number>\d+)\];/.match(line)
       end
+    end
+  end
+
+  def store_vector(identifyer, vector)
+    if identifyer == 'x0'
+      @x0 = vector
+    elsif identifyer == 'u0'
+      @u0 = vector
+    end
+  end
+
+  def store_matrix(identifyer, matrix)
+    if identifyer == 'A'
+      @A = matrix
+    elsif identifyer == 'B'
+      @B = matrix
+    elsif identifyer == 'C'
+      @C = matrix
+    elsif identifyer == 'D'
+      @D = matrix
     end
   end
 end
