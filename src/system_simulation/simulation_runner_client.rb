2936f88bd8ca817ea91bfecc12f0b6a41ed512c2
Lukas Rambold <lukas@rambold.io>
2020-08-10 17:02:40 +0200

    fixed stupid mistake

--- src/system_simulation/simulation_runner_client.rb
+++ src/system_simulation/simulation_runner_client.rb
@@ -4,7 +4,7 @@ require 'net/http'
 require 'uri'
 require_relative 'animation_data_sample.rb'
 
-SIMULATION_RUNNER_HOST = "http://ec2-18-158-146-2.eu-central-1.compute.amazona>
+SIMULATION_RUNNER_HOST = "http://ec2-18-158-146-2.eu-central-1.compute.amazonaws.com:8080".freeze
 
 class SimulationRunnerClient
   def self.update_model(json_string)
