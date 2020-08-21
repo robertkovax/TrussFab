2417f65d043b57861c73c72ab4cb76538aa9baa4
birneamstiel <luckifri@yahoo.de>
2020-08-21 14:55:22 +0200

    Adjust weights of pipes/ball hubs to measured values.

--- src/system_simulation/server/modelica_configuration.rb
+++ src/system_simulation/server/modelica_configuration.rb
@@ -1,7 +1,7 @@
 module ModelicaConfiguration
   # Constants for Modelica model generation
-  NODE_WEIGHT_KG = 0.3
-  PIPE_WEIGHT_KG_PER_M = 1
+  NODE_WEIGHT_KG = 0.2
+  PIPE_WEIGHT_KG_PER_M = 0.758
   SPRING_CONSTANT = 7000
   STATIC_SPRING_CONSTANT = 1_000_000
   POINT_MASS_GENERATION_ENABLED = true
