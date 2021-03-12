0b7ebb33f9757a5015fc970cd5f4eff660805284
origamidance <origamidance@gmail.com>
2021-03-12 18:21:40 +0100

    finished a working version of rigid group detection

--- src/julia/src/rigidGroupDetection.jl
+++ src/julia/src/rigidGroupDetection.jl
@@ -10,7 +10,7 @@ using GraphPlot
 
 # relies on vertecies having for n vertecies 1:n as lables for vertecies
 # g = TrussFab.import_trussfab_file("src/julia/test_models/chair.json", false)
-g = TrussFab.import_trussfab_file("src/julia/test_models/bird.json", false)
+g = TrussFab.import_trussfab_file("src/julia/test_models/dragon.json", false)
 # g = TrussFab.import_trussfab_file("src/julia/test_models/seesaw_3.json", false)
 # g = TrussFab.import_trussfab_file("src/julia/test_models/simple_seesaw.json", false)
 # g = TrussFab.import_trussfab_file("src/julia/test_models/sketchup_tetrahedron.json", false)
@@ -34,7 +34,7 @@ gplot(g)
 
 # in Cpp et al one would just do i*3 but julia uses indexing starting with one.
 # so it has to be offset this way otherwise, the first index would be i * 3 = 1 * 3 = 3 and not 1
-offset_index = (i, o) -> (i-1)*o +1
+offset_index = (i, o) -> (i - 1) * o + 1
 
 # function get_rigidity_matrix(g)
 #     total_fixed=0
@@ -61,26 +61,26 @@ offset_index = (i, o) -> (i-1)*o +1
 # end
 
 function get_rigidity_matrix(g)
-    R = zeros(ne(g), nv(g)*3)
+    R = zeros(ne(g), nv(g) * 3)
     for (i, e) in enumerate(edges(g))
         tuple = get_prop(g, e.src, :init_pos) - get_prop(g, e.dst, :init_pos)
         # println("tuple=",tuple)
-        R[i, offset_index(e.src, 3):offset_index(e.src, 3)+2] = tuple
-        R[i, offset_index(e.dst, 3):offset_index(e.dst, 3)+2] = -tuple
+        R[i, offset_index(e.src, 3):offset_index(e.src, 3) + 2] = tuple
+        R[i, offset_index(e.dst, 3):offset_index(e.dst, 3) + 2] = -tuple
     end
 
     # add boundry conditions ie. set fixed nodes
     for fixed_vertex in filter(n -> get_prop(g, n, :fixed), vertices(g))
         # new_row = (1:nv(g) .|> i -> i == fixed_vertex ? [1,0,0] : zeros(3)) |> Iterators.flatten |> Iterators.collect
         # show(new_row)
-        new_row=zeros(nv(g)*3)
-        new_row[3*(fixed_vertex-1)+1]=1
+        new_row = zeros(nv(g) * 3)
+        new_row[3 * (fixed_vertex - 1) + 1] = 1
         R = vcat(R, new_row')
-        new_row=zeros(nv(g)*3)
-        new_row[3*(fixed_vertex-1)+2]=1
+        new_row = zeros(nv(g) * 3)
+        new_row[3 * (fixed_vertex - 1) + 2] = 1
         R = vcat(R, new_row')
-        new_row=zeros(nv(g)*3)
-        new_row[3*(fixed_vertex-1)+3]=1
+        new_row = zeros(nv(g) * 3)
+        new_row[3 * (fixed_vertex - 1) + 3] = 1
         R = vcat(R, new_row')
         # R[ne(g)+3*(i-1)+1,3*(i-1)+1]=1
         # R[ne(g)+3*(i-1)+2,3*(i-1)+2]=1
@@ -95,6 +95,53 @@ end
 
 test_rigidity(g)
 
+function get_rigid_groups(g)
+    RV = Any[]
+    RG = Any[]
+    R = get_rigidity_matrix(g)
+    Rs = R' * R
+    Vdim = nv(g)
+    if rank(Rs) == Vdim * 3
+        push!(RG, Array(1:size(Rs)[1]))
+        return RG
+    else
+        n = nullspace(Rs)
+        nullspace_ids = (sum(reshape((sum(abs.(n) .> 1e-10, dims=2)) .> 1e-10, (3, :)), dims=1)) .> 0
+        push!(RG, .!nullspace_ids)
+        push!(RV, zeros(Vdim * 3))
+    end
+    Rl = R
+    while sum(nullspace_ids) > 0
+        if rank(Rs) == Vdim * 3 - 1
+            n = nullspace(Rs)
+            rigid_group_ids = (sum(reshape((sum(abs.(n) .> 1e-10, dims=2)) .> 1e-10, (3, :)), dims=1)) .> 0
+            push!(RG, rigid_group_ids)
+            eigen_vector = eigvecs(Rs)[:,1]
+            push!(RV, eigen_vector)
+            fix_id = findfirst(rigid_group_ids)[2]
+            new_row = zeros(Vdim * 3)
+            new_row[3 * (fix_id - 1) + 1] = 1
+            new_row[3 * (fix_id - 1) + 2] = 1
+            new_row[3 * (fix_id - 1) + 3] = 1
+            R = vcat(R, new_row')
+            nullspace_ids = nullspace_ids - rigid_group_ids
+
+            Rl = R
+
+        else
+            rigid_group_ids = (sum(reshape((sum(abs.(n) .> 1e-10, dims=2)) .> 1e-10, (3, :)), dims=1)) .> 0
+            fix_id = findfirst(rigid_group_ids)[2]
+            new_row = zeros(Vdim * 3)
+            new_row[3 * (fix_id - 1) + 1] = 1
+            new_row[3 * (fix_id - 1) + 2] = 1
+            new_row[3 * (fix_id - 1) + 3] = 1
+            Rl = vcat(Rl, new_row')
+        end
+        Rs = Rl' * Rl
+    end
+    return [RG,RV]
+end
+
 nv(g)
 
 function get_symmetric_rigidity_matrix(g)
@@ -103,26 +150,20 @@ function get_symmetric_rigidity_matrix(g)
     return transpose(R) * R
 end
 
-plot_matrix = m -> heatmap(m, color = :greys)
+plot_matrix = m -> heatmap(m, color=:greys)
 
 
 R = get_rigidity_matrix(g)
-test_rigidity(g)
-# println("R=",R)
-# println("rank=",rank(R))
-Rs = get_symmetric_rigidity_matrix(g)
-# println("Rs=",Rs)
-println("eigen value=",eigvals(Rs))
-eigen_vectors=eigvecs(Rs)
-# println("eigen vector=",eigen_vectors)
-@time n = nullspace(Rs)
+Rs = R' * R
+# println("eigen value=",eigvals(Rs))
+eigen_vectors = eigvecs(Rs)
+# # println("eigen vector=",eigen_vectors)
+# @time n = nullspace(Rs)
 
 gplot(g)
 plot_matrix(R)
 plot_matrix(Rs)
 plot_matrix(eigen_vectors)
 plot_matrix(n)
-# plot_matrix(eigvecs(Rs))
-row_1=eigen_vectors[1,:]
-row_2=eigen_vectors[2,:]
-print(dot(row_1,row_2))
+# print(eigen_vectors[:,1])
+(RG, RV) = get_rigid_groups(g)
