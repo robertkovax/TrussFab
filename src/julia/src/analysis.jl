cbfacc620f657393d6f951ba27330043dd16b433
Lukas Rambold <lukas@rambold.io>
2021-03-17 16:24:14 +0100

    new get_amplitude implementation that takes the points that are furthest apart

--- src/julia/src/analysis.jl
+++ src/julia/src/analysis.jl
@@ -20,41 +20,23 @@ function get_dominant_frequency(sol, vertex_id)
 end
 
 function get_amplitude(sol, vertex_id)
-    # go through timeseries -> set start point 
-    # -> advance as long as next vector is further away from start than previous one
-    # -> when this is not the case anymore the path is considered one amplitude
-    # -> check  whether this amplitued exceeds the currently longest amplitude
-    # ↺ for entire time series
+    # finds the two points that are furthest apart
+
     largest_aplitude = 0
     largest_aplitude_start_index = nothing
     largest_aplitude_end_index = nothing
-    timeseries = sol[vertex_id*6-5:vertex_id*6-3, :]
+    timeseries = sol[vertex_id*6-5:vertex_id*6 - 4, :]
 
-    start_pos = nothing
-    start_index = nothing
-    prev_pos = nothing
-    current_amplitude_length = 0
-
-    for (index, pos) in enumerate(eachcol(timeseries))
-        if start_pos === nothing
-            start_pos = pos
-            start_index = index
-        elseif prev_pos === nothing
-            current_amplitude_length = norm(start_pos - pos)
-            prev_pos = pos
-        elseif norm(start_pos - pos) < norm(start_pos - prev_pos)
-            # terminal condition
-            if largest_aplitude < current_amplitude_length
-                largest_aplitude = current_amplitude_length
-                largest_aplitude_start_index = start_index
-                largest_aplitude_end_index = index
+    for (index1, pos1) in enumerate(eachcol(timeseries))
+        for (index2, pos2) in enumerate(eachcol(timeseries))
+            if (index1 >= index2)
+                continue
             end
-            start_pos = nothing
-            start_index = nothing
-            prev_pos = nothing
-        else
-            current_amplitude_length += norm(prev_pos - pos)
-            prev_pos = pos
+            if (norm(pos1 .- pos2) > largest_aplitude)
+                largest_aplitude = norm(pos1 .- pos2)
+                largest_aplitude_start_index = index1
+                largest_aplitude_end_index = index2
+            end
         end
     end
     return largest_aplitude, (largest_aplitude_start_index, largest_aplitude_end_index)
