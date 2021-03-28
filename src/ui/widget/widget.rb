90378c0eec26ceb569708e6c84c5a0c771cb1bfc
birneamstiel <luckifri@yahoo.de>
2021-03-28 23:05:34 +0200

    Center widget text.

--- src/ui/widget/widget.rb
+++ src/ui/widget/widget.rb
@@ -1,6 +1,6 @@
 class Widget
   attr_reader :position, :current_state
-  LABEL_HEIGHT = 1.5
+  LABEL_HEIGHT = 2.0
 
 
   def initialize(position, valid_states, image_path)
@@ -23,14 +23,17 @@ class Widget
   def create_image
     # TODO: Make sure that we can use @group here
     #
-    image_position = Geom::Point3d.new(0,0,15.cm)
+    image_width = 7
     image = @group.entities.add_image(
-    @image_path,
-      image_position,
-      7,
+      @image_path,
+      Geom::Point3d.new,
+      image_width,
     )
-    rotation = Geometry.rotation_transformation(Geom::Vector3d.new(0, 0, 1), Geom::Vector3d.new(0, -1, 0), image_position)
+    rotation = Geometry.rotation_transformation(Geom::Vector3d.new(0, 0, 1), Geom::Vector3d.new(0, -1, 0), Geom::Point3d.new)
     image.transform! rotation
+
+    translation = Geom::Transformation.translation(Geom::Vector3d.new(-(image.bounds.width/2),  0, 15.cm))
+    image.transform! translation
     image.parent.behavior.always_face_camera = true
   end
 
@@ -47,16 +50,16 @@ class Widget
       # label_definition.behavior.always_face_camera = true
       entities = label_definition.entities
       bold = index == @current_state
-      success = entities.add_3d_text(state, TextAlignCenter, "Arial", bold, false, LABEL_HEIGHT, 0.0, 0, true, 0.1)
+      success = entities.add_3d_text(state, TextAlignLeft, "Arial", bold, false, LABEL_HEIGHT, 0.0, 0, true, 0.1)
 
       rotation = Geometry.rotation_transformation(Geom::Vector3d.new(0, 0, 1), Geom::Vector3d.new(0, -1, 0), Geom::Point3d.new(0, 0, 0))
-      internal_translation = Geom::Transformation.translation(Geom::Vector3d.new(0,  0, index * LABEL_HEIGHT * 1.2))
+      internal_translation = Geom::Transformation.translation(Geom::Vector3d.new(-label_definition.bounds.width/2,  0, index * LABEL_HEIGHT * 1.2))
       translation = Geom::Transformation.translation(@position)
       # translation = Geom::Transformation.translation(Geom::Vector3d.new(-label_definition.bounds.width / 2,  0, 0))
       transform = internal_translation * rotation
 
       instance = parent.entities.add_instance(label_definition, transform)
-      instance.material = index == @current_state ? Sketchup::Color.new(20, 20, 20) : Sketchup::Color.new(70, 70, 70)
+      instance.material = index == @current_state ? Sketchup::Color.new(20, 20, 20) : Sketchup::Color.new(150, 150, 150)
 
       @group.transformation = translation
       group_entities.add_instance(parent, Geom::Transformation.new)
